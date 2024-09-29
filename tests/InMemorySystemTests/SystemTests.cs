da10d959f5efd05e558c8640ee25e1bca389ed11
ianoflynnautomation <ian.oflynn11@gmail.com>
2024-09-29 11:07:42 +0200

    instal browsers from code

--- tests/InMemorySystemTests/SystemTests.cs
+++ tests/InMemorySystemTests/SystemTests.cs
@@ -1,24 +1,35 @@
-
 namespace SystemTests;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
-public class Tests : PageTest 
+public class Tests : PageTest
 {
     public ServerFixture _fixture;
+
+
 
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
-       _fixture = new ServerFixture();
+        _fixture = new ServerFixture();
+
+        var exitCode = Microsoft.Playwright.Program.Main(new[] { "install" });
+        if (exitCode != 0)
+        {
+            Console.WriteLine("Failed to install browsers");
+            Environment.Exit(exitCode);
+        }
+
+        Console.WriteLine("Browsers installed");
     }
 
     [Test]
     public async Task Test()
-    {      
+    {
+
         await Page.GotoAsync(_fixture.ServerAddress);
         var title = await Page.TitleAsync();
-        Assert.Equals("Catalog - Microsoft.eShopOnWeb", title);  
+        Assert.Equals("Catalog - Microsoft.eShopOnWeb", title);
     }
 
     [OneTimeTearDown]
