025856602384cf06272c1d5f4ac2525db8c89848
ianoflynnautomation <ian.oflynn11@gmail.com>
2024-11-16 17:39:04 +0100

    fix

--- tests/DockerSystemTests/BaseTest.cs
+++ tests/DockerSystemTests/BaseTest.cs
@@ -1,21 +1,10 @@
-﻿using Playwright.DotNet.Configuration;
-using Playwright.DotNet.Infra;
-using Playwright.DotNet.Services;
-using Autofac;
-using Playwright.DotNet.DI;
-using Playwright.DotNet.Fixtures;
-using Playwright.DotNet.Enums;
+﻿using Playwright.DotNet.Fixtures;
 
 namespace EShopOnWeb.DockerSystemTests;
 
-public class BaseTest
+public class BaseTest : PageTest
 {
     protected SystemTestFixture _fixture;
-    protected App _app;
-    protected TestExecutionEngine _testExecutionEngine;
-    protected EShopOnWebApp _eShopOnWebApp;
-    protected ContainerBuilder _builder; 
-    protected IContainer _container;
     protected TestContext TestContext => TestContext.CurrentContext;
 
     [OneTimeSetUp]
@@ -27,41 +16,13 @@ public class BaseTest
     [SetUp]
     public void SetUp()
     {  
-        _builder = new ContainerBuilder();
-        var config = new BrowserConfiguration()
-        {
-            BrowserType = BrowserTypes.Chromium,
-        };
 
-        _builder.RegisterInstance(config).As<BrowserConfiguration>().SingleInstance();
-        _testExecutionEngine = new TestExecutionEngine();
-        _builder.RegisterInstance(_testExecutionEngine).AsSelf();
-
-         _fixture.Logger.Information($"Start test {TestContext.Test.FullName}");
-        _testExecutionEngine.StartBrowser(config, _builder);
-        
-        DISetup.RegisterPageObjects(_builder);
-        DISetup.RegisterServices(_builder);
-        DISetup.RegisterApp(_builder);
-
-        _container = _builder.Build();
-        ServiceLocator.SetContainer(_container);
-
-        _eShopOnWebApp = new EShopOnWebApp(_container);
-        _fixture.Logger.Information("Navigating to the web server url {WebServerUrl}", _fixture.SystemTestHost.WebServerUrl);
-
-        _app = ServiceLocator.Resolve<App>();
-        _app.Navigation.Navigate(_fixture.SystemTestHost.WebServerUrl);
     }
 
     [TearDown]
     public void TearDown()
     {
-        _fixture.Logger.Information($"End test {TestContext.Test.FullName}");
-        _testExecutionEngine.Dispose(_container);
-        _app = ServiceLocator.Resolve<App>();
-        _app.Dispose();
-        _container.Dispose();
+
     }
 
     [OneTimeTearDown]
