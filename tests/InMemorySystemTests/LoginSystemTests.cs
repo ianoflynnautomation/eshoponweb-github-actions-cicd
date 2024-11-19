9e22847d3298411ccb34fb47bbe4b0963dab3b2a
ianoflynnautomation <ian.oflynn11@gmail.com>
2024-11-19 22:30:24 +0100

    fix

--- tests/InMemorySystemTests/LoginSystemTests.cs
+++ tests/InMemorySystemTests/LoginSystemTests.cs
@@ -8,17 +8,7 @@ namespace EShopOnWeb.InMemorySystemTests;
 [Parallelizable(ParallelScope.Self)]
 
 [TestFixture]
-public class LoginOrderSystemTests : InMemoryBaseTest
+public class LoginOrderSystemTests : InMemorySystemTestsBase
 {
-    // [Test]
-    // public async Task Login_Success_UserJourney()
-    // {        
-    //     var headerSection = new HeaderSection(Page);
-    //     var loginPage = new LoginPage(Page);
-
-    //     await headerSection.OpenLogin();
-    //     await loginPage.Login("admin@microsoft.com", "Pass@word1", false);
-   
-    // }
 
 }
